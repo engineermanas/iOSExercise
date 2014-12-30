4d35f92640d4221d26c822ece699302d063e9833
Manasa Parida <manasaparida@ManasaOSX.local>
2014-12-30 14:52:20 +0530

    Initial Commit

---
+++ iOSExercise/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSExercise
+//
+//  Created by Manasa Parida on 30/12/14.
+//  Copyright (c) 2014 Manas Parida. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOSExercise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSExercise
+//
+//  Created by Manasa Parida on 30/12/14.
+//  Copyright (c) 2014 Manas Parida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
